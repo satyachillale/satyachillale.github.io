cc71baaad44c274fdac30d174b4f42dc03989608
satyachillale <satya.narayana2406@gmail.com>
2025-05-23 05:39:44 +0000

    Deploying to gh-pages from @ satyachillale/satyachillale.github.io@278ce9945f51fa9ee18a643f751f851762c7fab3 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -414,11 +414,16 @@ ninja.data = [{
           description: "Analyzes financial data and sentiment, revealing interconnections across stocks, Bitcoin, loans.",
           section: "Projects",handler: () => {
               window.location.href = "/projects/4_project/";
+            },},{id: "projects-reinforcement-learning-in-rocket-league-rl-multi-reward-system",
+          title: 'Reinforcement Learning in Rocket League (RL²) Multi reward system',
+          description: "A deep dive into how reward design shapes the instincts of AI agents on the Rocket League field",
+          section: "Projects",handler: () => {
+              window.location.href = "/projects/5_project/";
             },},{id: "projects-sentiment-amp-capital-the-data-behind-financialmovements",
           title: 'SENTIMENT &amp;amp; CAPITAL, THE DATA BEHIND FINANCIALMOVEMENTS',
           description: "Analyzes financial data and sentiment, revealing interconnections across stocks, Bitcoin, loans.",
           section: "Projects",handler: () => {
-              window.location.href = "/projects/5_project/";
+              window.location.href = "/projects/6_project/";
             },},{
         id: 'social-email',
         title: 'email',
