9607279fef7ead2e1bd899379a718f3e84b9b27e
satyachillale <satya.narayana2406@gmail.com>
2025-05-23 05:37:17 +0000

    Deploying to gh-pages from @ satyachillale/satyachillale.github.io@fbd670f9fdcdbb78c64a6e109c071cec9ea6ea3d 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -414,6 +414,11 @@ ninja.data = [{
           description: "Analyzes financial data and sentiment, revealing interconnections across stocks, Bitcoin, loans.",
           section: "Projects",handler: () => {
               window.location.href = "/projects/4_project/";
+            },},{id: "projects-sentiment-amp-capital-the-data-behind-financialmovements",
+          title: 'SENTIMENT &amp;amp; CAPITAL, THE DATA BEHIND FINANCIALMOVEMENTS',
+          description: "Analyzes financial data and sentiment, revealing interconnections across stocks, Bitcoin, loans.",
+          section: "Projects",handler: () => {
+              window.location.href = "/projects/5_project/";
             },},{
         id: 'social-email',
         title: 'email',
